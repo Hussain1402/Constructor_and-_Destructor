caa39525654e099892cddbe042f96dc243360df0
Hussain Tayyebi <141639385+Hussain1402@users.noreply.github.com>
2024-09-05 11:38:23 +0530

    Update Constructor_inside_class.cpp

--- Constructor_inside_class.cpp
+++ Constructor_inside_class.cpp
@@ -5,12 +5,16 @@ class date{
     int m;
     int y;
     
+    
     public:
     date(){
+        cout<<"Enter the day: ";
+        cin>>d;
+        cout<<"Enter the month: ";
+        cin>>m;
+        cout<<"Enter the year: ";
+        cin>>y;
         cout<<"Toda's date is: "<<endl;
-        d=4;
-        m=9;
-        y=2024;
         cout<<d<<"/"<<m<<"/"<<y;
     }
 };
@@ -21,6 +25,9 @@ int main(){
 }
 
 /*
+Enter the day: 4
+Enter the month: 9
+Enter the year: 2024
 Toda's date is: 
 4/9/2024
 */
